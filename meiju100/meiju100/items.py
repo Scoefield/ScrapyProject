b355a533c917eb76260540ef67855e4453181fe7
Scoefield <scoefield@163.com>
2018-10-24 10:50:33 +0800

    add some item

--- meiju100/meiju100/items.py
+++ meiju100/meiju100/items.py
@@ -11,4 +11,7 @@ import scrapy
 class Meiju100Item(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    movname = scrapy.Field()
+    movstatu = scrapy.Field()
+    movstation = scrapy.Field()
+    movupdate = scrapy.Field()
