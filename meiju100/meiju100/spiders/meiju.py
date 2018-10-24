72091b3bbafe3ac269f03baf7d8d54fab2632328
Scoefield <scoefield@163.com>
2018-10-24 11:59:12 +0800

    modify meiju.py

--- meiju100/meiju100/spiders/meiju.py
+++ meiju100/meiju100/spiders/meiju.py
@@ -9,13 +9,28 @@ class MeijuSpider(scrapy.Spider):
     start_urls = ['http://www.meijutt.com/new100.html']
 
     def parse(self, response):
-        li_list = response.xpath('//ul[@class="top-list fn-clear"]/li')
+        li_list = response.xpath('//ul[@class="top-list  fn-clear"]/li')
 
         for sub in li_list:
             item = Meiju100Item()
             item['movname'] = sub.xpath('./h5/a/text()').extract_first()
-            item['movurl'] = sub.xpath('./h5/a/@href').extract_first()
-            item['movstatu'] = sub.xpath('./span/font/text()').extract_first()
-            item['movstation'] = sub.xpath('./span/font/text()').extract_first()
-            item['movupdate'] = sub.xpath('./div/font/text()').extract_first()
+            item['movurl'] = response.urljoin(sub.xpath('./h5/a/@href').extract_first())
+            item['movstatu'] = sub.xpath('./span[1]/font/text()').extract_first()
+            item['movstation'] = sub.xpath('./span[2]/text()').extract_first()
+            item['movupdate'] = sub.xpath('./div[2]/text()').extract_first()
+
+            if item['movstatu']:
+                pass
+            else:
+                item['movstatu'] = sub.xpath('./span[1]/text()').extract()
+
+            if item['movstation']:
+                pass
+            else:
+                item['movstation'] = [u'未知']
+            
+            if item['movupdate']:
+                pass
+            else:
+                item['movupdate'] = sub.xpath('./div[2]/font/text()').extract()
             yield item
