6202f2e87ac867df74f3486b2011abd194e54d3a
Scoefield <scoefield@163.com>
2018-10-24 11:18:36 +0800

    modify meiju.py

--- meiju100/meiju100/spiders/meiju.py
+++ meiju100/meiju100/spiders/meiju.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from meiju100.items import Meiju100Item
 
 
 class MeijuSpider(scrapy.Spider):
@@ -8,4 +9,13 @@ class MeijuSpider(scrapy.Spider):
     start_urls = ['http://www.meijutt.com/new100.html']
 
     def parse(self, response):
-        pass
+        li_list = response.xpath('//ul[@class="top-list fn-clear"]/li')
+
+        for sub in li_list:
+            item = Meiju100Item()
+            item['movname'] = sub.xpath('./h5/a/text()').extract_first()
+            item['movurl'] = sub.xpath('./h5/a/@href').extract_first()
+            item['movstatu'] = sub.xpath('./span/font/text()').extract_first()
+            item['movstation'] = sub.xpath('./span/font/text()').extract_first()
+            item['movupdate'] = sub.xpath('./div/font/text()').extract_first()
+            yield item
