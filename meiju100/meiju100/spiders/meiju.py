b18b18adb146f8970b6acb5e78428a46c936653c
Scoefield <scoefield@163.com>
2018-10-24 10:53:46 +0800

    modify start_url

--- meiju100/meiju100/spiders/meiju.py
+++ meiju100/meiju100/spiders/meiju.py
@@ -5,7 +5,7 @@ import scrapy
 class MeijuSpider(scrapy.Spider):
     name = "meiju"
     allowed_domains = ["www.meijutt.com"]
-    start_urls = ['http://www.meijutt.com/']
+    start_urls = ['http://www.meijutt.com/new100.html']
 
     def parse(self, response):
         pass
